d830ec508abb399e2900493fd349ca8cd5605273
caoduythanh <caoduythanhcantho@gmail.com>
2017-06-03 00:06:35 +0700

    Modified: - Add augement data

--- Src/Main.py
+++ Src/Main.py
@@ -1,19 +1,25 @@
 from Models.LSTMModel import *
 from Utils.DatasetUtil import *
+import random
 
 # TRAINING CONFIG
 NUM_EPOCH          = 10
-MAX_ITERATION      = 200000
+MAX_ITERATION      = 1000000
 LEARNING_RATE      = 0.0001
-DISPLAY_FREQUENCY  = 100;            DISPLAY_CONTENT = 'LearningRate = %f, Epoch = %d, iteration = %d, cost = %f'
-VALIDATE_FREQUENCY = 10000
-SAVE_FREQUENCY     = 1000
+DISPLAY_FREQUENCY  = 100;            DISPLAY_CONTENT = 'LearningRate = %f, Epoch = %d, iteration = %d, cost = %f. Best valid cost = %f'
+VALIDATE_FREQUENCY = 5000
+SAVE_FREQUENCY     = 2500
 
 # EARLY STOPPING
 PATIENCE = 50000
 PATIENCE_INCREASE = 2
-IMPROVEMENT_THRESHOLD = 0.995
+IMPROVEMENT_THRESHOLD = 0.9999995
 
+# AUGEMENT SETTING
+ROT_ANGLE_LIMIT = 15. / 180 * 3.14
+TRANS_LIMIT     = 0.2
+SHEAR_LIMIT     = 0.5
+SCALE_LIMIT     = 0.2
 
 # PATH SETTINGS
 DATASET_PATH  = '../Dataset/English/'
@@ -25,7 +31,7 @@ STATE_PATH    = '../Pretrained/CurrentState.pkl'
 # NETWORK CONFIG
 INPUT_SIZE  = 2
 HIDDEN_SIZE = 256
-OUTPUT_SIZE = 64
+OUTPUT_SIZE = 62
 
 # GLOBAL VARIABLES
 Dataset   = None
@@ -37,6 +43,10 @@ Model = None
 def ReadDataset():
     global Dataset
     Dataset = DatasetUtil(datasetPath = DATASET_PATH)
+
+    file = open('../Dataset/strokes_training_data.cpkl')
+
+    file.close()
 
 
 ###############################
@@ -53,7 +63,9 @@ def CreateModel():
 ###########################
 #      VALID MODEL        #
 ###########################
-def ValidModel(LSTMModel, validData):
+def ValidModel(Model, validData):
+    print ('----------------------------------- VALIDATION -----------------------------------------------------------')
+
     AllCosts = []
     iter  = 0
     epoch = 0
@@ -72,11 +84,14 @@ def ValidModel(LSTMModel, validData):
         char = char.reshape((1,))
 
         iter += 1
-        cost = Model.PredFunc(input, char)
+        cost = Model.ValidFunc(input, char)
         AllCosts.append(cost)
 
         if iter % DISPLAY_FREQUENCY == 0:
-            print (DISPLAY_CONTENT % (LEARNING_RATE, epoch, iter, numpy.mean(AllCosts)))
+            print (DISPLAY_CONTENT % (LEARNING_RATE, epoch, iter, numpy.mean(AllCosts), epoch))
+
+    print ('Valid cost = %f' % (numpy.mean(AllCosts)))
+    print ('----------------------------------- VALIDATION (DONE) ----------------------------------------------------')
 
     return numpy.mean(AllCosts)
 
@@ -84,7 +99,9 @@ def ValidModel(LSTMModel, validData):
 #      TRAIN MODEL        #
 ###########################
 def TrainModel():
-    global Dataset, Model
+    global Dataset, Model, \
+           PATIENCE, PATIENCE_INCREASE, \
+           IMPROVEMENT_THRESHOLD
 
     # Load record
     AllCosts = []
@@ -98,11 +115,12 @@ def TrainModel():
     # Load model
     if CheckFileExist(STATE_PATH, throwError = False):
         file = open(STATE_PATH)
-        LSTMModel.LoadState(file)
+        Model.LoadState(file)
         file.close()
         print ('Load state !')
 
     trainData = Dataset.TrainData
+    validData = Dataset.ValidData
     epoch = 0
     iter  = 0
     costs = []
@@ -121,16 +139,31 @@ def TrainModel():
             char  = trainSample['Char']
             char  = char.reshape((1,))
 
+            # for k in range(10):
+            #     plt.plot(input[:, 0, 1], -input[:, 0, 0])
+            #     plt.axis([-0.5, 0.5, -0.5, 0.5])
+            #     plt.show()
+            #
+            #     plt.plot(auInput[:, 0, 1], -auInput[:, 0, 0])
+            #     plt.axis([-0.5, 0.5, -0.5, 0.5])
+            #     plt.show()
+            #
+            #     print ('%s' % Dataset.Character[char])
+            #     plt.pause(2)
+            #     plt.close()
+
+            auInput = AugmentData(input)
+
             iter += 1
-            cost = Model.TrainFunc(input, char)
+            cost = Model.TrainFunc(auInput, char)
             costs.append(cost)
 
             if iter % DISPLAY_FREQUENCY == 0:
-                print (DISPLAY_CONTENT % (LEARNING_RATE, epoch, iter, numpy.mean(costs)))
+                print (DISPLAY_CONTENT % (LEARNING_RATE, epoch, iter, numpy.mean(costs), BestCost))
                 costs = []
 
             if iter % VALIDATE_FREQUENCY == 0:
-                validCost = ValidModel()
+                validCost = ValidModel(Model, validData)
                 if validCost < BestCost:
                     if validCost < BestCost * IMPROVEMENT_THRESHOLD:
                         PATIENCE = max(PATIENCE, iter * PATIENCE_INCREASE)
@@ -138,14 +171,14 @@ def TrainModel():
                     BestCost = validCost
                     # Save best model
                     file = open(BEST_PATH, 'wb')
-                    LSTMModel.SaveModel(file)
+                    Model.SaveModel(file)
                     file.close()
                     print ('Save best model !')
 
             if iter % SAVE_FREQUENCY == 0:
                 # Save model
-                file = open(SAVE_PATH, 'wb')
-                LSTMModel.SaveModel(file)
+                file = open(SAVE_PATH % (epoch, iter), 'wb')
+                Model.SaveModel(file)
                 file.close()
                 print ('Save model !')
 
@@ -158,10 +191,64 @@ def TrainModel():
 
                 # Save state
                 file = open(STATE_PATH, 'wb')
-                LSTMModel.SaveState(file)
+                Model.SaveState(file)
                 file.close()
                 print ('Save state !')
         epoch += 1
+
+# - Affine matrix --------------------------------------------------------------------------------------------------
+def RotateMatrix(alpha):
+    return numpy.asarray([[math.cos(alpha), -math.sin(alpha), 0],
+                          [math.sin(alpha),  math.cos(alpha), 0],
+                          [              0,                0, 1]], dtype = 'float32')
+
+def TranslateMatrix(dx, dy):
+    return numpy.asarray([[1, 0, dx],
+                          [0, 1, dy],
+                          [0, 0,  1]], dtype = 'float32')
+
+def ScaleMatrix(scaleX, scaleY):
+    return numpy.asarray([[scaleX,      0, 0],
+                          [     0, scaleY, 0],
+                          [     0,      0, 1]], dtype = 'float32')
+
+def ShearMatrix(dx, dy):
+    return numpy.asarray([[ 1, dy, 0],
+                          [ 0,  1, 0],
+                          [ 0,  0, 1]], dtype = 'float32').dot(
+           numpy.asarray([[ 1, 0, 0],
+                          [dx, 1, 0],
+                          [ 0, 0, 1]], dtype='float32')
+           )
+
+def AugmentData(input):
+    alpha = random.uniform(-ROT_ANGLE_LIMIT, ROT_ANGLE_LIMIT)
+    dx    = random.uniform(-TRANS_LIMIT, TRANS_LIMIT)
+    dy    = random.uniform(-TRANS_LIMIT, TRANS_LIMIT)
+    scaleX = random.uniform(1 - SCALE_LIMIT, 1 + SCALE_LIMIT)
+    scaleY = random.uniform(1 - SCALE_LIMIT, 1 + SCALE_LIMIT)
+    shearX = random.uniform(-SHEAR_LIMIT, SHEAR_LIMIT)
+    shearY = random.uniform(-SHEAR_LIMIT, SHEAR_LIMIT)
+
+    rotMax   = RotateMatrix(alpha)
+    transMax = TranslateMatrix(dx, dy)
+    scaleMax = ScaleMatrix(scaleX, scaleY)
+    shearMax = ShearMatrix(shearX, shearY)
+
+    input = input.reshape((input.shape[0], input.shape[2]))
+
+    ones  = numpy.ones((input.shape[0], 1), dtype = 'float32')
+    input = numpy.concatenate((input, ones), axis=1)
+
+    input = rotMax.dot(shearMax).dot(scaleMax).dot(transMax).dot(input.T)
+    # input = transMax.dot(scaleMax).dot(shearMax).dot(rotMax).dot(input.T)
+    # input = shearMax.dot(input.T)
+    input = input.T
+    input = input[:, :2]
+    input = input.reshape((input.shape[0], 1, input.shape[1]))
+    return input
+
+
 
 #############################################################################################
 #                                                                                           #
